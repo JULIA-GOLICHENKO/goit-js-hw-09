97aa6a366b14c5a0f5db379ff65d79b15c8d41e9
JULIA-GOLICHENKO <goli4enko.jul@gmail.com>
2023-01-04 10:53:40 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
 
+let timerId = null;
+
+startBtn.addEventListener("click", () => {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    timerId = setInterval(() => {
+        document.body.style.background = getRandomHexColor();
+      }, 1000);
+    });
+  
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
+stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+});
+// Напиши скрипт, який після натискання кнопки «Start», раз на секунду змінює колір фону <body> 
+// на випадкове значення, використовуючи інлайн стиль. Натисканням на кнопку «Stop» зміна кольору 
+// фону повинна зупинятися.
+// Враховуй, що на кнопку «Start» можна натиснути нескінченну кількість разів. Зроби так, щоб доки 
+// зміна теми запущена, кнопка «Start» була неактивною (disabled).
+// onclick="this.disabled=true
